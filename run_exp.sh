695449930b201a27fbe627a0599b92bdc4b11ac0
mihahauke <kempka.michal@gmail.com>
2018-11-12 13:44:08 +0100

    Corrected run exp

--- run_exp.sh
+++ run_exp.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-ACTUAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+ACTUAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${ACTUAL_DIR}
 NAME="optim_`hostname`"
 IMAGE_TAG="optim"
 
+docker_script="docker/build_n_run.sh"
 
-./docker/slurm.sh "mnist" test.py -c connfig/exp/exp_mnist.yml
-./docker/slurm.sh "bank" test.py -c connfig/exp/exp_bank.yml
-./docker/slurm.sh "census" test.py -c connfig/exp/exp_census.yml
-./docker/slurm.sh "covtype" test.py -c connfig/exp/exp_covtype.yml
-./docker/slurm.sh "madelon" test.py -c connfig/exp/exp_madelon.yml
-./docker/slurm.sh "shuttle" test.py -c connfig/exp/exp_shuttle.yml
+./docker/slurm.sh "mnist" ${docker_script} ./test.py -c connfig/exp/exp_mnist.yml
+./docker/slurm.sh "bank" ${docker_script} ./test.py -c connfig/exp/exp_bank.yml
+./docker/slurm.sh "census" ${docker_script} ./test.py -c connfig/exp/exp_census.yml
+./docker/slurm.sh "covtype" ${docker_script} ./test.py -c connfig/exp/exp_covtype.yml
+./docker/slurm.sh "madelon" ${docker_script} ./test.py -c connfig/exp/exp_madelon.yml
+./docker/slurm.sh "shuttle" ${docker_script} ./test.py -c connfig/exp/exp_shuttle.yml
