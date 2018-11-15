28404645c10f20d0ef44354c6b3e7c79fa84f307
mihahauke <kempka.michal@gmail.com>
2018-11-15 16:31:33 +0100

    Extended gitignore.

---
+++ short_names.py
@@ -0,0 +1,34 @@
+from scinol import *
+import tensorflow as tf
+
+from cocob import COCOBOptimizer, COCOBOptimizer0
+
+nag = NAGOptimizer
+scinol = ScinolOptimizer
+scinol2 = Scinol2Optimizer
+prescinol = PreScinolOptimizer
+prescinol2 = PreScinol2Optimizer
+prescinoldl = PreScinolDLOptimizer
+prescinol2dl = PreScinol2DLOptimizer
+cocob = COCOBOptimizer
+cocob0 = COCOBOptimizer0
+
+rmsprop = tf.train.RMSPropOptimizer
+adagrad = tf.train.AdagradOptimizer
+adam = tf.train.AdamOptimizer
+adadelta = tf.train.AdadeltaOptimizer
+
+
+def sgd(learning_rate, use_locking=False, name="SGD", decay=None,*args,**kwargs):
+    if decay is not None:
+        t = tf.train.get_or_create_global_step()
+        t = tf.assign_add(t, 1)
+        t = tf.to_float(t)
+        if decay == "linear":
+            learning_rate = learning_rate / t
+        elif decay == "sqrt":
+            learning_rate = learning_rate / tf.sqrt(t)
+        else:
+            raise ValueError("Unsupported decay for sgd: {}".format(decay))
+
+    return tf.train.GradientDescentOptimizer(learning_rate, use_locking, name)
