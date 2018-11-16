f796bc0261e8d8ef3121838e9ccb82bfe4831043
mihahauke <kempka.michal@gmail.com>
2018-11-16 02:40:02 +0100

    Added script to plot exp.

--- exp0_plot.py
+++ exp0_plot.py
@@ -15,6 +15,23 @@ import traceback
 plt.style.use("ggplot")
 from tqdm import tqdm
 
+names_dict = {
+    "adam": "Adam",
+    "adagrad": "AdaGrad",
+    "gradientdescent": "SGD",
+    "prescinol": "SI-Cw",
+    "prescinol2": "SI-Full",
+    "nag": "NAG"
+}
+colors_dict = {
+    "adam": "black",
+    "adagrad": "m",
+    "gradientdescent": "limegreen",
+    "prescinol": "red",
+    "prescinol2": "deepskyblue",
+    "nag": "gold"
+}
+
 
 # TODO maybe it's a stupid idea, maybe I can do it with pandas?
 # it seems that not really, different datasets and test/train sets will have different dimensionality
@@ -204,6 +221,78 @@ def plot_with_std(tree,
     plt.xlabel(x_axis_label)
 
 
+def plot_with_std_v2(tree,
+                     tag_sets,
+                     title="TODO",
+                     y_axis="TODO",
+                     verbose=False,
+                     x_axis_label="# iterations",
+                     **kwargs):
+    data = []
+    steps = None
+    min_runs_num = np.inf
+    min_steps_num = np.inf
+    max_runs_num = 0
+    max_steps_num = 0
+    for tags in tag_sets:
+        series = tree.get(tags)
+        steps = series[0, 0, :]
+        values = series[:, 1, :]
+        data.append(values)
+        min_runs_num = min(min_runs_num, values.shape[0])
+        min_steps_num = min(min_steps_num, values.shape[1])
+        max_runs_num = max(max_runs_num, values.shape[0])
+        max_steps_num = max(max_steps_num, values.shape[1])
+
+    if verbose:
+        if min_steps_num != max_steps_num:
+            # TODO
+            pass
+        if min_runs_num != max_runs_num:
+            # TODO
+            pass
+
+    steps = steps[0:min_steps_num]
+    # TODO it's a workaround for different number of runs and points
+    if len(data) > 1:
+        data = [d[0:min_runs_num, 0:min_steps_num] for d in data]
+        data = np.stack(data, axis=2)
+        tag_sets = [t[-1] for t in tag_sets]
+    else:
+        tag_sets = None
+        data = data[0]
+    default_kwargs = {
+        "err_style": "ci_band",
+        "ci": "sd",
+    }
+    default_kwargs.update(kwargs)
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=UserWarning)
+        # Group tags:
+        tag_groups = defaultdict(list)
+        for i, tag in enumerate(tag_sets):
+            for tag_group_key in names_dict:
+                if tag.startswith(tag_group_key):
+                    tag_groups[tag_group_key].append(i)
+
+        ax = None
+        for tag, indices in tag_groups.items():
+            for i, di in enumerate(indices):
+                ax = sns.tsplot(data[:, :, di],
+                                time=steps,
+                                # value=y_axis,
+                                condition=tag,
+                                legend=i == 0,
+                                ax=ax,
+                                color=colors_dict[tag],
+                                **default_kwargs
+                                )
+    plt.title(title)
+    plt.xlabel(x_axis_label)
+    plt.locator_params(nbins=8)
+
+
 def save_plot(path, extension="pdf"):
     if not extension.startswith("."):
         extension = "." + extension
@@ -225,21 +314,8 @@ if __name__ == "__main__":
                         default="graphs")
     parser.add_argument("--log_dir",
                         default="tb_logs")
-    parser.add_argument("-i", "--interactive",
-                        action="store_true",
-                        help="TODO")
-    parser.add_argument("-f", "--filters",
-                        nargs="*",
-                        default=[],
-                        help="TODO")
-    parser.add_argument("-x", "--exclude",
-                        nargs="*",
-                        default=[],
-                        help="TODO")
-    parser.add_argument("-l", "--list",
-                        action="store_true")
     parser.add_argument("--extension", "-e",
-                        default="pdf")
+                        default="png")
     parser.add_argument("--verbose", "-v",
                         action="store_true",
                         default=False)
@@ -248,59 +324,58 @@ if __name__ == "__main__":
     all_files = glob.glob('{}/**/*events*'.format(args.log_dir), recursive=True)
     tree = Tree(verbose=args.verbose)
 
-    tree.load(all_files, args.filters, args.exclude)
+    filters = ["adam", "adagrad", "prescinol", "nag", "gradientdescent", "prescinol2"]
+    tree.load(all_files, filters)
 
-    if args.list:
-        tree.print()
-        exit(0)
-    if args.interactive:
-        raise NotImplementedError()
-
-    # Plot everything separately
+    # Plots everything separately
     all_keys = list(tree.random_access_data.keys())
     print("Saving graphs to: '{}'".format(args.output_dir))
 
-    for key in tqdm(all_keys, leave=False):
-        dataset, mode, architecture, algo = key
-        try:
-            plot_with_std(tree,
-                          tag_sets=[key],
-                          y_axis="cross entropy",
-                          title="{}: {}".format(dataset, algo),
-                          err_style="unit_traces")
+    # for key in tqdm(all_keys, leave=False):
+    #     dataset, mode, architecture, algo = key
+    #     try:
+    #         plot_with_std(tree,
+    #                       tag_sets=[key],
+    #                       y_axis="cross entropy",
+    #                       title="{}: {}".format(dataset, algo),
+    #                       err_style="unit_traces")
+    #
+    #         save_plot(os.path.join(args.output_dir, dataset, mode, architecture, algo),
+    #                   extension=args.extension)
+    #     except Exception as ex:
+    #         print("Failed for: {}".format(key))
+    #         print("Data shape: {}".format(tree.get(key).shape))
+    #         if args.verbose:
+    #             print(" ============== EXCEPTION ============")
+    #             print(ex)
+    #             traceback.print_exc()
+    #             print(" =====================================")
+    #         exit(0)
 
-            save_plot(os.path.join(args.output_dir, dataset, mode, architecture, algo),
-                      extension=args.extension)
-        except Exception as ex:
-            print("Failed for: {}".format(key))
-            print("Data shape: {}".format(tree.get(key).shape))
-            if args.verbose:
-                print(" ============== EXCEPTION ============")
-                print(ex)
-                traceback.print_exc()
-                print(" =====================================")
-            exit(0)
-
-    # TODO it's ugly af ... refactor it ....
-    # TODO and defaq does it dooo XD?
     joined_keys = {}
     for d, m, a, algo in all_keys:
         if not (d, m, a) in joined_keys:
             joined_keys[(d, m, a)] = []
         joined_keys[(d, m, a)].append([d, m, a, algo])
 
+    # Plot version 1
+
+    # for [d, m, a], tag_set in tqdm(joined_keys.items(), leave=False):
+    #     tag_set = sorted(tag_set, key=lambda x: x[3])
+    #     plot_with_std(tree,
+    #                   tag_sets=tag_set,
+    #                   y_axis="cross entropy",
+    #                   title="{} {} {}".format(d, m, a))
+    #     save_plot(os.path.join(args.output_dir, d) + "_v1",
+    #               extension=args.extension)
+
+    # Plot version 2
+
     for [d, m, a], tag_set in tqdm(joined_keys.items(), leave=False):
-        try:
-            tag_set = sorted(tag_set, key=lambda x: x[3])
-            plot_with_std(tree,
-                          tag_sets=tag_set,
-                          y_axis="cross entropy",
-                          title="{} {} {}".format(d, m, a))
-            save_plot(os.path.join(args.output_dir, "{}_{}".format(d, m), a),
-                      extension=args.extension)
-        except Exception as ex:
-            print("Failed for: {} + tags".format([d, m, a]))
-            if args.verbose:
-                print(" ============== EXCEPTION ============")
-                traceback.print_exc()
-                print(" =====================================")
+        tag_set = sorted(tag_set, key=lambda x: x[3])
+        plot_with_std_v2(tree,
+                         tag_sets=tag_set,
+                         y_axis="cross entropy",
+                         title="{} {} {}".format(d, m, a))
+        save_plot(os.path.join(args.output_dir, d) + "_v2",
+                  extension=args.extension)
