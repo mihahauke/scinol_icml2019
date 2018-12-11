08687e910c82ea82e0f59b063d951e487cfb93d8
mihahauke <kempka.michal@gmail.com>
2018-12-11 19:03:36 +0100

    slurm is stupid

--- bash_scripts/run_icml_batch128.sh
+++ bash_scripts/run_icml_batch128.sh
@@ -3,7 +3,8 @@
 ACTUAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${ACTUAL_DIR}/..
 
-docker_script="~/optimizers/docker/build_n_run.sh"
+# Don't add parentheses, '~' in there won't be resolved correctly
+docker_script=~/optimizers/docker/build_n_run.sh
 configs_dir="configs/exp_icml_batch128"
 
 ./docker/slurm.sh "mnist" ${docker_script} ./test.py -c ${configs_dir}/"mnist.yml"
