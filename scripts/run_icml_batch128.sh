63c171516305d3d56763977a86bafa0a430ca69c
mihahauke <kempka.michal@gmail.com>
2018-12-26 13:20:26 +0100

    Added configs for b128

--- scripts/run_icml_batch128.sh
+++ scripts/run_icml_batch128.sh
@@ -5,12 +5,11 @@ cd ${ACTUAL_DIR}/..
 
 # Don't add parentheses, '~' in there won't be resolved correctly
 docker_script=~/optimizers/docker/build_n_run.sh
-configs_dir="configs/exp_icml_batch128"
+configs_dir="configs/exp_b128"
 
-./docker/slurm.sh "mnist" ${docker_script} ./test.py -c ${configs_dir}/"mnist.yml"
-./docker/slurm.sh "bank" ${docker_script} ./test.py -c ${configs_dir}/"bank.yml"
-./docker/slurm.sh "census" ${docker_script} ./test.py -c ${configs_dir}/"census.yml"
-./docker/slurm.sh "covtype" ${docker_script} ./test.py -c ${configs_dir}/"covtype.yml"
-./docker/slurm.sh "madelon" ${docker_script} ./test.py -c ${configs_dir}/"madelon.yml"
-./docker/slurm.sh "shuttle" ${docker_script} ./test.py -c ${configs_dir}/"shuttle.yml"
-./docker/slurm.sh "artificial" ${docker_script} ./test.py -c ${configs_dir}/"art.yml"
+./docker/slurm.sh "mnist128" ${docker_script} ./test.py -c ${configs_dir}/"mnist.yml"
+./docker/slurm.sh "bank128" ${docker_script} ./test.py -c ${configs_dir}/"bank.yml"
+./docker/slurm.sh "census128" ${docker_script} ./test.py -c ${configs_dir}/"census.yml"
+./docker/slurm.sh "covtype128" ${docker_script} ./test.py -c ${configs_dir}/"covtype.yml"
+./docker/slurm.sh "madelon128" ${docker_script} ./test.py -c ${configs_dir}/"madelon.yml"
+./docker/slurm.sh "shuttle128" ${docker_script} ./test.py -c ${configs_dir}/"shuttle.yml"
